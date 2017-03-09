82b7c777cd290a502c54a2bb040d212e4bf26641
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-03-09 10:14:15 -0600

    server.js added

---
+++ server/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+module.exports = app;
+
+app.use(bodyParser.urlencoded());
+app.use( bodyParser.json() );
+
+
+app.post('/', function (req, res) {
+  console.log('post /');
+});
+
+app.get('/', function (req, res) {
+  console.log('get /');
+});
+
+var port = process.env.PORT || 4040;
+app.listen(port);
+console.log("Listening on port " + port);
