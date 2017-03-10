8f48c8fc9cf29f840794fce974eba9a5382f4de7
Nathan <nathan.turinski@gmail.com>
2017-03-09 22:30:38 -0600

    Server serves index.html from a GET request

--- public/client/index.js
+++ public/client/index.js
@@ -1,9 +1,23 @@
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('app')
-// );
-
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('app')
-);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- above meta tags for bootstrap-->
+    <title>Pretzel</title>
+    <link rel="stylesheet" href="client/lib/react-bootstrap/react-bootstrap.js">
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="client/lib/jquery/dist/jquery.js"></script>
+    <script src="client/lib/react/react.js"></script>
+    <script src="client/lib/react/react-dom.js"></script>
+    <script src="client/lib/lodash/lodash.js"></script>
+  </head>
+  <body>
+    <div id="app"></div>
+    <script type="text/jsx" src="./../compiled/public/client/components/Log.jsx"></script>
+    <script type="text/jsx" src="./../compiled/public/client/components/Word.jsx"></script>
+    <script type="text/jsx" src="./../compiled/public/client/components/App.jsx"></script>
+    <script type="text/jsx" src="./../compiled/public/client/index.js"></script>
+  </body>
+</html> 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 module.exports = app;
 
 // app.use(bodyParser.urlencoded());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use( bodyParser.json() );
+app.use(bodyParser.json() );
 
+//app.use(express.static(path.resolve(__dirname, '..', 'public', 'client')));
 
 app.post('/', function (req, res) {
   console.log('post /');
@@ -16,10 +18,11 @@ app.post('/', function (req, res) {
 
 app.get('/', function (req, res) {
   console.log('get /');
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
   res.sendStatus(200); // for testing purposes
 });
 
-var port = process.env.PORT || 4040;
+var port = process.env.PORT || 8000;
 if(!module.parent){
     app.listen(port);
 }
