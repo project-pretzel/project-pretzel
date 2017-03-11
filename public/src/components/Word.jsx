1ea235039e221cab9ccfe539c92ccc5247a9a779
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-03-11 15:26:55 -0600

    cleaned up the css that we didnt need

--- public/src/components/Word.jsx
+++ public/src/components/Word.jsx
@@ -6,13 +6,14 @@ class Word extends React.Component{
   render(){
     return(
       <div>
-        <div class="container" id="maincontent" tabindex="-1">
-          <div class="row">
+        <div class="container" id="WordMap" tabindex="-1">
+          <div class="">
             <div class="col-lg-12">
               <div class="intro-text">
                 <h1 class="name">WORD MAP GOES HERE</h1>
               </div>
             </div>
+
           </div>
         </div>
       </div>
