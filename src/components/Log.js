7334a7566d607955f3ceaaaa961d2b0a9750062c
Nathan <nathan.turinski@gmail.com>
2017-03-14 15:10:49 -0500

    Saves user information on login.  Does not persist or save into database

--- src/components/Log.js
+++ src/components/Log.js
@@ -5,21 +5,38 @@ export default class Log extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: true};
+    this.state = {
+      gid: null,
+      email: null,
+      givenName: null,
+      familyName: null,
+      imageUrl: null,
+      loggedIn: false
+    };
     // This binding is necessary to make `this` work in the callback
   }
 
   responseGoogle(response){
-    console.log(response);
+    
+    this.setState({
+      gid: response.profileObj.googleId,
+      email: response.profileObj.email,
+      givenName: response.profileObj.givenName,
+      familyName: response.profileObj.familyName,
+      imageUrl: response.profileObj.imageUrl,
+      loggedIn: true
+      });
+    console.log(this.state);
+
   }
 
   render() {
     return (
-      <div onClick={this.handleClick}>
+      <div>
         <GoogleLogin
-        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={this.responseGoogle}
+        clientId="791653946192-gial2vpbjr0pdtg6k90a9jrfja3c5mgl.apps.googleusercontent.com"
+        buttonText={!(this.state.loggedIn) ? 'Login' : 'Hi, ' + this.state.givenName} 
+        onSuccess={this.responseGoogle.bind(this)}
         onFailure={this.responseGoogle}
         />
       </div> 
