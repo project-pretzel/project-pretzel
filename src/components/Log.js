f2563573f055342f74b4a8870728665b31e98c4b
Nathan <nathan.turinski@gmail.com>
2017-03-14 14:23:27 -0500

    Login component included in header.  index.html moved to Layout.js'

--- src/components/Log.js
+++ src/components/Log.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 
 export default class Log extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+    // This binding is necessary to make `this` work in the callback
+  }
+
+  responseGoogle(response){
+    console.log(response);
+  }
+
   render() {
     return (
-      <div>
-        Log test
+      <div onClick={this.handleClick}>
+        <GoogleLogin
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+        />
       </div> 
     );
   }
