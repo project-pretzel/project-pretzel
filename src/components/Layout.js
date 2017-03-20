92aeae300e46f68d27032d8533d3bc5b96b49426
Nathan <nathan.turinski@gmail.com>
2017-03-20 17:02:14 -0500

    Changes circle sizes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,7 +12,7 @@ export default class Layout extends React.Component {
       <nav id="mainNav" className="navbar navbar-default navbar-custom affix-top">
         <div className="container">
           <div className="navbar-header">
-            <a className="navbar-brand" href="#page-top">Project Pretzel</a>
+            <a className="navbar-brand" href="#page-top">Pretzel: Be in the loop</a>
           </div>
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav navbar-right">
